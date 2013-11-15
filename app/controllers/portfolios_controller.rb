582f9f95c4680d6723cdd3915dd68be7daf6d117
emteajay <mtjjoyce@gmail.com>
2013-11-15 17:31:24 -0500

    added show for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,15 +1,30 @@
 class PortfoliosController < ApplicationController
   
   def index
-  	#@portfolios = Portfolio.all
+  	@portfolios = Portfolio.all
   end
 
   def new
-  	#@portfolios = Portfolio.new
+  	@portfolios = Portfolio.new
   end	
 
   def create
-  	render text: params[:portfolio].inspect
+  	@portfolio = Portfolio.new(portfolio_params)
+  	if @portfolio.save
+  		redirect_to @portfolio
+  	else
+  		render 'new'
+  	end		
+  end	
+
+  def show
+  	@portfolio = Portfolio.find(params[:id])
+  end	
+
+  private
+
+  def portfolio_params
+  	params.require(:portfolio).permit(:fund, :commitment, :paid_in, :distributed, :adjustment, :net_asset_value)  	
   end	
 
 end
