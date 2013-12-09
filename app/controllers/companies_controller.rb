8b73ae46402aa1c95ec36c9d7812842f5155fdd2
emteajay <mtjjoyce@gmail.com>
2013-12-08 19:20:12 -0500

    added companies to database through model

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,4 +3,5 @@ class CompaniesController < ApplicationController
 	def index
 		@company = Companies.search_for(params[:q])
 	end
+
 end
--- app/models/companies.rb
+++ app/models/companies.rb
@@ -4,26 +4,44 @@ class Companies < ActiveRecord::Base
 
 	private
 
-	def self.search_for(query) #self is like company.search_for
+	# def self.search_for(query) #self is like company.search_for
 		
-		# our_company = Companies.where(name: query) #use when stored to database. DONT DO YET BECAUSE DATABASE WON'T BE UPDATED
-		# unless our_company
+	# 	# our_company = Companies.where(name: query) #use when stored to database. DONT DO YET BECAUSE DATABASE WON'T BE UPDATED
+	# 	# unless our_company
+	# 	company = Crunchbase::Company.find(query)
+	# 		# @our_company = Companies.new #create a new instance of the company
+	# 		# @our_company.name = company.name
+	# 		# @our_company.permalink = company.permalink
+	# 		# @our_company.phone_number = company.phone_number
+	# 		# @our_company.total_money_raised = company.total_money_raised
+	# 		# @our_company.category_code = company.category_code
+	# 		# @our_company.founded_year = company.founded_year
+	# 		# @our_company.homepage_url = company.homepage_url
+	# 		# @our_company.number_of_employees = company.number_of_employees
+	# 		# @our_company.twitter_username = company.twitter_username
+	# 		# @our_company.overview = company.overview
+	# 		# @our_company.save		
+	# 		# @our_company
+	# 	# end
+	# 	#will save to database
+	# 	#puts "Crunchbase company class is #{company.class}" #will only show up in irb
+	# end
+
+
+	def self.search_for(query) #self is like company.search_for
 		company = Crunchbase::Company.find(query)
-			# @our_company = Companies.new #create a new instance of the company
-			# @our_company.name = company.name
-			# @our_company.permalink = company.permalink
-			# @our_company.phone_number = company.phone_number
-			# @our_company.total_money_raised = company.total_money_raised
-			# @our_company.category_code = company.category_code
-			# @our_company.founded_year = company.founded_year
-			# @our_company.homepage_url = company.homepage_url
-			# @our_company.number_of_employees = company.number_of_employees
-			# @our_company.twitter_username = company.twitter_username
-			# @our_company.overview = company.overview
-			# @our_company.save	
-			# @our_company
-		# end
-		#will save to database
-		#puts "Crunchbase company class is #{company.class}" #will only show up in irb
+		saved_company = Companies.where(name: query) #use when stored to database. DONT DO YET BECAUSE DATABASE WON'T BE UPDATED
+		saved_company = Companies.find_or_create_by(
+			name: company.name  ,
+			permalink: company.permalink ,
+			phone_number: company.phone_number ,
+			total_money_raised: company.total_money_raised ,
+			category_code: company.category_code ,
+			founded_year: company.founded_year ,
+			homepage_url: company.homepage_url ,
+			number_of_employees: company.number_of_employees ,
+			twitter_username: company.twitter_username ,
+			overview: company.overview
+			)	 
 	end
 end
