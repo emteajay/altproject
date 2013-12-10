3b577ed15c6adbfc67d2e4274842bc8abc0436df
emteajay <mtjjoyce@gmail.com>
2013-12-10 18:11:14 -0500

    final presentation

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,16 +4,21 @@ class CompaniesController < ApplicationController
 		@company = Companies.search_for(params[:q])
 	end
 
-	def show
+	# def show
 		
-	end
+	# end
 
-	def create
+	# def create
+	# 	@investments = Investment.find(params[:invesments_id])
+	# 	@company = @investment.companies.create(params[:companies]
+	# 	redirect_to companies_path(@company)
+	# end
 
-	end
-
-	def destroy
-
-	end	
+	# def destroy
+	# 	@company = Companies.find(params[:invesments_id])
+	# 	@company = @company.companies.find(params[:id])
+	# 	@company.destroy
+	# 	redirect_to companies_path(@company)
+	# end	
 
 end
