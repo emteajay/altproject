32571897efb637cc21898760131051deb5dea180
emteajay <mtjjoyce@gmail.com>
2013-12-12 14:36:23 -0500

    added funding rounds to company view

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,7 +1,6 @@
 class Investment < ActiveRecord::Base
 	validates :fund, presence: true
 	validates :fund, presence: true
-	belongs_to :user
 	belongs_to :financial_organization
 	has_many :companies
 end
